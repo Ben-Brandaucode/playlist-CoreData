aab6954266acddf8038300237bfb45d3de70ce2c
Benjamin Brandau <codingbrandau@gmail.com>
2020-10-07 23:41:20 -0600

    setup

---
+++ PlaylistCoreData/PlaylistCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaylistCoreData
+//
+//  Created by Ben Brandau Brandau on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
